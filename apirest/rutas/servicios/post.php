88b42b58853e026315127b9bd1fec5a69665e19f
Roberto Carlos López Játiva <81062997+rclopezjativa@users.noreply.github.com>
2022-07-16 10:34:56 -0500

    Programar el método setDatos
    Programar la invocación del método setDatos del archivo post.controlador.php

--- apirest/rutas/servicios/post.php
+++ apirest/rutas/servicios/post.php
@@ -1,22 +1,23 @@
 <?php
-
 	require_once "modelos/conexion.php";
-	 
-	if (isset($_POST)) {
-		$columnas = array();
+	require_once "controladores/post.controlador.php";
+	if (isset($_POST)) {											// Si el requerieminto enviado es con el método POST
+		$columnas = array();										// Se guardan los nombres de los campos enviados en el formulario del requerimiento.
 		foreach(array_keys($_POST) as $k => $v){
 			array_push($columnas, $v);
 		}
-		if (empty(Conexion::getColumnas(explode("?", $r_array[1])[0], $columnas))) {
+		if (empty(Conexion::getColumnas(explode("?", $r_array[$i_EndPoint])[0], $columnas))) {
 			$jfile = array (
 				'status' => 400,
-				'result' => 'Los campos del formulario no coinciden con los de la base de datos',
+				'error' => $GLOBALS["err_code"],
+				'result' => $GLOBALS["err_mensaje"], 				// 'Los campos del formulario no coinciden con los de la base de datos',
 				'format' => 'MVC'
 			);
 			echo json_encode($jfile, http_response_code($jfile["status"]));
 			return;
 		}
-		$resp = new postControlador();
+		$resp = new postControlador(); 
+		$resp->setDatos(explode("?", $r_array[$i_EndPoint])[0], $_POST, $verLog);
 	}
 
 ?>
