1bdd8d17b48984ba058ae27fe987e75ca7829321
Roberto Carlos López Játiva <81062997+rclopezjativa@users.noreply.github.com>
2022-07-16 10:54:17 -0500

    Reorganización
    Se agrega el nombre de la tabla al mensaje de error y se reorganiza y comenta el archivo conexion.php

--- apirest/modelos/conexion.php
+++ apirest/modelos/conexion.php
@@ -1,42 +1,40 @@
 <?php
 	class Conexion {
-		static public function infoDatabase(){
-			$infoDB = array(
-				"host" => "localhost",
-				"dbname" => "bolsaempleo",
-				"usuario" => "root",
-				"clave" => ""
-			);	
+		static public function infoDatabase(){						// Declarar datos de conexión a la base de datos
+			$infoDB = array( 	"host" => "localhost",
+								"dbname" => "bolsaempleo",
+								"usuario" => "root",
+								"clave" => ""
+							);	
 			return $infoDB;
 		}
-		
-		static public function conecta() {
-			try{
-				$enlace = new PDO("mysql:host=".Conexion::infoDatabase()["host"].";dbname=".Conexion::infoDatabase()["dbname"],
+		static public function conecta() {							// Método de conexión a la base de datios
+			try	{ $enlace = new PDO("mysql:host=".Conexion::infoDatabase()["host"].";dbname=".Conexion::infoDatabase()["dbname"],
 								  Conexion::infoDatabase()["usuario"],
 								  Conexion::infoDatabase()["clave"]
-								 );
+				);
 				$enlace->exec("set name utf8");
-			}catch(PDOException $e){ die("Error: " . $e->getMessage()); }
+			} catch (PDOException $e)								// Si se presenta error en la conexión 
+			  { die("Error: " . $e->getMessage()); }
 			return $enlace;
 		}
-		
-		static public function getColumnas($tabla, $columnas){
+		static public function getColumnas($tabla, $columnas){		// Función que verifica la existencia de la tabla y las columnas
 			$db = Conexion::infoDatabase()["dbname"];
-			$v = Conexion::conecta()											// Retornamos la conexión en el caso de ser válida
+			$v = Conexion::conecta()								// Retornamos la conexión en el caso de ser válida
 			->query("SELECT COLUMN_NAME as item FROM information_schema.columns WHERE table_schema = '$db' AND table_name = '$tabla'")
 			->fetchAll(PDO::FETCH_OBJ);
-			if(empty($v)){
+			if(empty($v)){											// Si no retorna el esquema significa que no existe la tabla 
 				$GLOBALS["err_mensaje"] = "No existe tabla: $tabla"; $GLOBALS["err_code"] = 100;
 				return null;
 			} else {
 				if($columnas[0] == "*") { array_shift($columnas); }
 				$s = 0;
-				foreach($v as $h => $vi) { $s += in_array($vi->item, $columnas); }
-				if($s == count($columnas)) {
-					return $v;
-				} else {
-					$GLOBALS["err_mensaje"] = "Los nombres de columna no coinciden"; $GLOBALS["err_code"] = 101;
+				foreach($v as $h => $vi)							 
+				{ $s += in_array($vi->item, $columnas); }			// Se verifican si existen las columnas 
+				if($s == count($columnas)) { return $v; } 			// Si todas las columnas coinciden, no existe error
+				else {												// Si no coinciden todos los campos consultados, existe error
+					$GLOBALS["err_mensaje"] = "Los nombres de campo no coinciden en la tabla: $tabla"; 
+					$GLOBALS["err_code"] = 101;
 					return null;
 				}
 			}
