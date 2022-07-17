c555a473a9e1eebba12521f9acd6268c710d2d9c
Roberto Carlos López Játiva <81062997+rclopezjativa@users.noreply.github.com>
2022-07-17 11:51:20 -0500

    Parametrización del EndPoint
    En la línea 14 se parametriza la validación del EndPoint ($i_EndPoint)
    Se agregan comentarios a la línea 1 y 2
    Se organizan los comentarios
    Linea 34 vacía se la suprime

--- apirest/rutas/rutas.php
+++ apirest/rutas/rutas.php
@@ -1,27 +1,27 @@
 <?php
-	$r_method = ""; $verLog = false;
-	if (isset($_GET["verLog"])) { $verLog = true; }
-	$i_EndPoint = 1;											// Configurar según el ambiente publicado
+	$r_method = ""; $verLog = false;					// Almacenar el método a procesar en la API y declarar la variable verLog para seguimiento
+	if (isset($_GET["verLog"])) { $verLog = true; }				// Si no se envía como GET (verLog) no se muestra el seguimiento
+	$i_EndPoint = 1;							// Configurar según el ambiente publicado
 	$r_array = explode("/", $_SERVER['REQUEST_URI']);			// Capturo la dirección URL utilizada
-	$r_array = array_filter($r_array);							// Cada elemento entre los caracteres "/" en un arreglo
-	if (count($r_array) == 0) {									// Si no se envía EndPonit
+	$r_array = array_filter($r_array);					// Cada elemento entre los caracteres "/" en un arreglo
+	if (count($r_array) == 0) {						// Si no se envía EndPonit
 		$jfile = array (
 		'status' => 404,
 		'result' => 'Not found',
 		'format' => 'MVC'
 		);
 	} else {
-		if(count($r_array) == 1) {								// Si se envía una petición URI con formato host/endpoint
+		if(count($r_array) == $i_EndPoint) {				// Si se envía una petición URI con formato host/endpoint
 			switch ($_SERVER["REQUEST_METHOD"]) {
-				case "GET": 									// Detectar que es un método GET
+				case "GET": 					// Detectar que es un método GET
 					$r_method = "GET";
-					include "servicios/get.php";				// Enrutar al archivo de procesamiento del método GET
+					include "servicios/get.php";		// Enrutar al archivo de procesamiento del método GET
 					return;
-				case "POST": 									// Detectar que es un método POST
+				case "POST": 					// Detectar que es un método POST
 					$r_method = "POST"; 
-					include "servicios/post.php";				// Enrutar al archivo de procesamiento del método POST
+					include "servicios/post.php";		// Enrutar al archivo de procesamiento del método POST
 					return;
-				case "PUT": 	$r_method = "PUT"; break;		// Detectar que es un método PUT
+				case "PUT": 	$r_method = "PUT"; break;	// Detectar que es un método PUT
 				case "DELETE": 	$r_method = "DELETE"; break;	// Detectar que es un método DELETE
 			}
 		} else {
@@ -31,7 +31,6 @@
 				'format' => 'MVC'
 			);
 		}
-			
 	}
 	return;
 ?>
